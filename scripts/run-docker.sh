0d02754b88dfa76c238c8f492f26cbab54bf4adf
Michael Smith <Michael.Smith@vml.com>
2016-08-16 13:14:53 -0500

    Make the run-docker command launch the browser with the correct port.

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -11,4 +11,9 @@ DOCKER_COMPOSE="docker-compose -f ${DIR}/../.docker/docker-compose.yml"
 printf "${YELLOW}Starting Docker servers....${NC}"
 docker-compose -f ${DIR}/../.docker/docker-compose.yml build
 `${DOCKER_COMPOSE} up -d`
+
+port_ssl=`docker port docker_app_1 443`
+port_ssl=${port_ssl#*:}
+
+`open https://localhost:${port_ssl}`
 exit 0
