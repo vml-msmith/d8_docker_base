f6d27a9336b4e13720293b043a90b5dcbacd3571
Michael Smith <Michael.Smith@vml.com>
2016-08-16 13:07:41 -0500

    Add a run docker script.

---
+++ scripts/run-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_COMPOSE="docker-compose -f ${DIR}/../.docker/docker-compose.yml"
+
+## Startup the Docker servers. Ensures a new build first.:Q
+printf "${YELLOW}Starting Docker servers....${NC}"
+docker-compose -f ${DIR}/../.docker/docker-compose.yml build
+`${DOCKER_COMPOSE} up -d`
+exit 0
